ae9500e66ed86730d5fa5a044162f3c022b4112a
zhouliyfly <zhouliyfly@gmail.com>
2018-09-11 15:00:19 +0800

    代码结构修改为策略模式

--- myui.py
+++ myui.py
@@ -6,9 +6,8 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QColorDialog, QFontDialog, QTextEdit, QFileDialog
-from script import read_table_and_cacl
+from PyQt5 import QtCore, QtWidgets
+from my_action import MyAction
 from PyQt5 import sip
 
 
@@ -56,13 +55,12 @@ class Ui_Form(object):
         self.pushButton = QtWidgets.QPushButton(Form)
         self.pushButton.setGeometry(QtCore.QRect(30, 210, 75, 23))
         self.pushButton.setObjectName("pushButton")
-        # self.textBrowser = QtWidgets.QTextBrowser(Form)
-        # self.textBrowser.setEnabled(True)
-        # self.textBrowser.setGeometry(QtCore.QRect(220, 200, 161, 81))
-        # self.textBrowser.setObjectName("textBrowser")
         self.label_status = QtWidgets.QLabel(Form)
         self.label_status.setGeometry(QtCore.QRect(280, 260, 131, 20))
         self.label_status.setObjectName("label_status")
+
+        # 创建活动处理对象
+        self.active_action = MyAction(self)
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -80,71 +78,13 @@ class Ui_Form(object):
         self.label_output.setText(_translate("Form", "./"))
         self.button_output.setText(_translate("Form", "数据文件输出路径选择"))
         self.pushButton.setText(_translate("Form", "生成数据"))
+        # self.label_status.setText(" 状态栏")
 
-        # 初始化路径
-        # 输出路径默认为当前目录
-        self.table_output = "."
-        self.table_dingdan = ""
-        self.table_tejia = ""
-        self.table_manjian = ""
-        self.table_paths['dingdan'] = self.table_dingdan
-        self.table_paths['manjian'] = self.table_manjian
-        self.table_paths['tejia'] = self.table_tejia
-        self.table_paths['output'] = self.table_output
-
-        self.button_dingdan.clicked.connect(self.ui_action)
-        self.button_tejia.clicked.connect(self.ui_action)
-        self.button_manjian.clicked.connect(self.ui_action)
-        self.button_output.clicked.connect(self.ui_action)
-        self.pushButton.clicked.connect(self.ui_action)
-
-    def ui_action(self):
-        self.label_status.setText(" ")
-        sender = self.QWidget.sender()
-        if sender.text() == "导入订单表":
-            self.table_dingdan, _ = QFileDialog.getOpenFileName(self.QWidget, '打开文件', './', "Excel 文件(*.xls;*.xlsx)")
-            self.label_dingdan.setText(self.table_dingdan)
-            self.table_paths['dingdan'] = self.table_dingdan
-            print(self.table_dingdan)
-        elif sender.text() == "导入特价表":
-            self.table_tejia, _ = QFileDialog.getOpenFileName(self.QWidget, '打开文件', './', "Excel 文件(*.xls;*.xlsx)")
-            self.label_tejia.setText(self.table_tejia)
-            self.table_paths['tejia'] = self.table_tejia
-            print(self.table_tejia)
-        elif sender.text() == "导入满减表":
-            self.table_manjian, _ = QFileDialog.getOpenFileName(self.QWidget, '打开文件', './', "Excel 文件(*.xls;*.xlsx)")
-            self.label_manjian.setText(self.table_manjian)
-            self.table_paths['manjian'] = self.table_manjian
-            print(self.table_manjian)
-        elif sender.text() == "数据文件输出路径选择":
-            self.table_output = QFileDialog.getExistingDirectory(self.QWidget, '打开文件夹', './')
-            if self.table_output == "":
-                self.table_output = "."
-            self.label_output.setText(self.table_output)
-            self.table_paths['output'] = self.table_output
-            print(self.table_output)
-        elif sender.text() == "生成数据":
-            print("开始生成数据")
-
-            if self.table_paths['dingdan'] == "":
-                print("找不到订单表！")
-                self.label_status.setText("找不到订单表！")
-            elif self.table_paths['manjian'] == "" and self.table_paths['tejia'] == "":
-                print("找不到满减表或特价表！")
-                self.label_status.setText("找不到满减表或特价表！")
-            else:
-                print(self.table_paths)
-                sign1, sign2 = read_table_and_cacl(self.table_paths)
-                if sign1 == 1 and sign2 == 1:
-                    self.label_status.setText("已成功生成满减活动表和特价活动表！")
-                elif sign1 == 1:
-                    self.label_status.setText("已成功生成满减活动表！")
-                elif sign2 == 1:
-                    self.label_status.setText("已成功生成特价活动表！")
-                else:
-                    pass
-        else:
-            pass
+        self.button_dingdan.clicked.connect(self.active_action.ui_action)
+        self.button_tejia.clicked.connect(self.active_action.ui_action)
+        self.button_manjian.clicked.connect(self.active_action.ui_action)
+        self.button_output.clicked.connect(self.active_action.ui_action)
+        self.pushButton.clicked.connect(self.active_action.ui_action)
 
 
 if __name__ == "__main__":
