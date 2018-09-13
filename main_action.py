6360467bf7e5ab8dd619f4b98a13854b233afea1
zhouliyfly <zhouliyfly@gmail.com>
2018-09-13 14:40:03 +0800

    修改文件名和窗口应用，使用QMainWindow类，方便状态栏显示

---
+++ main_action.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module implementing action.
+"""
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5 import QtWidgets
+import active_calc
+
+
+class MainAction(QMainWindow):
+    def __init__(self, ui_main):
+        super().__init__()
+        self.ui_main = ui_main
+        # 初始化路径
+        # 输出路径默认为当前目录
+        self.table_paths = dict()
+        self.table_paths['dingdan'] = ""
+        self.table_paths['manjian'] = ""
+        self.table_paths['tejia'] = ""
+        self.table_paths['output'] = "."
+
+    def on_button_dingdan_clicked(self):
+        self.table_paths['dingdan'], _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                                               '打开文件', './',
+                                                                               "Excel 文件(*.xls;*.xlsx)")
+        self.ui_main.label_dingdan.setText(self.table_paths['dingdan'])
+
+    def on_button_tejia_clicked(self):
+        self.table_paths['tejia'], _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                                             "打开文件", "./",
+                                                                             "Excel 文件(*.xls;*.xlsx)")
+        self.ui_main.label_tejia.setText(self.table_paths['tejia'])
+
+    def on_button_manjian_clicked(self):
+        self.table_paths['manjian'], _ = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                                               "打开文件", "./",
+                                                                               "Excel 文件(*.xls;*.xlsx)")
+        self.ui_main.label_manjian.setText(self.table_paths['manjian'])
+
+    def on_button_output_clicked(self):
+        self.table_paths['output'] = QtWidgets.QFileDialog.getExistingDirectory(self,
+                                                                                "打开文件夹", "./")
+        if self.table_paths['output'] == "":
+            self.table_paths['output'] = "."
+
+        self.ui_main.label_output.setText(self.table_paths['output'])
+
+    def on_generate_data_button_clicked(self):
+        # 启动线程计算数据
+        self.ui_main.generate_data_button.setEnabled(False)  # 生成数据按钮暂时禁用
+        self.ui_main.thread.start()  # 开启线程执行数据计算
+        # self.ui_main.thread.join()
+        self.ui_main.generate_data_button.setEnabled(True)  # 生成数据按键恢复
+
+    def calc_all_data(self, trigger):
+        print("开始生成数据...")
+        trigger.emit("开始生成数据...")
+
+        if self.table_paths['dingdan'] == "":
+            print("找不到订单表！")
+            trigger.emit("找不到订单表！")
+            # self.ui_main.main_window.statusBar().showMessage("找不到订单表！")
+        elif self.table_paths['manjian'] == "" and self.table_paths['tejia'] == "":
+            print("找不到满减表或特价表！")
+            trigger.emit("找不到满减表或特价表！")
+            # self.ui_main.main_window.statusBar().showMessage("找不到满减表或特价表！")
+        else:
+            # 每次点击“生成数据”都会重新读取表格数据进行计算。acontext是临时局部变量，
+            # 若是想要读取表格数据和计算活动分开，需要分拆这两个步骤。并将获取的表格数据保存为类成员变量
+            acontext = active_calc.ActiveContext(self.table_paths)
+            status = acontext.active_calc()
+
+            # 此处待优化
+            if status:
+                trigger.emit("已成功生成活动表！")
+
+
+class WorkThread(QThread):
+    trigger = pyqtSignal(str)
+
+    def __init__(self, ui_main):
+        super().__init__()
+        self.ui_main = ui_main
+
+    def run(self):
+        self.ui_main.main_action.calc_all_data(self.trigger)
+        # self.ui_main.generate_data_button.setEnabled(True)  # 生成数据按键恢复
+        # 发出信号
+
---
+++ main_window.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+# from main_action import MainAction
+import main_action
+from PyQt5 import sip  # 为了打包exe文件包含的库
+
+
+class UiMainWindow(object):
+    def __init__(self, mian_window):
+        self.main_window = mian_window
+
+        # 初始化UI
+        self.setup_ui()
+
+        # 创建活动处理对象
+        self.main_action = main_action.MainAction(self)
+
+        # 创建线程
+        self.thread = main_action.WorkThread(self)
+        self.thread.trigger.connect(self.set_status)
+
+        # 初始化按键响应
+        self.button_dingdan.clicked.connect(self.main_action.on_button_dingdan_clicked)
+        self.button_tejia.clicked.connect(self.main_action.on_button_tejia_clicked)
+        self.button_manjian.clicked.connect(self.main_action.on_button_manjian_clicked)
+        self.button_output.clicked.connect(self.main_action.on_button_output_clicked)
+        self.generate_data_button.clicked.connect(self.main_action.on_generate_data_button_clicked)
+
+    def setup_ui(self):
+        self.main_window.setObjectName("main_window")
+        self.main_window.resize(558, 296)
+        self.centralWidget = QtWidgets.QWidget(self.main_window)
+        self.centralWidget.setObjectName("centralWidget")
+        self.formLayoutWidget = QtWidgets.QWidget(self.centralWidget)
+        self.formLayoutWidget.setGeometry(QtCore.QRect(40, 40, 491, 121))
+        self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.button_dingdan = QtWidgets.QPushButton(self.formLayoutWidget)
+        self.button_dingdan.setObjectName("button_dingdan")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.button_dingdan)
+        self.label_dingdan = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_dingdan.setObjectName("label_dingdan")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.label_dingdan)
+        self.label_manjian = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_manjian.setObjectName("label_manjian")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.label_manjian)
+        self.button_tejia = QtWidgets.QPushButton(self.formLayoutWidget)
+        self.button_tejia.setObjectName("button_tejia")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.button_tejia)
+        self.label_tejia = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_tejia.setObjectName("label_tejia")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.label_tejia)
+        self.button_manjian = QtWidgets.QPushButton(self.formLayoutWidget)
+        self.button_manjian.setObjectName("button_manjian")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.button_manjian)
+        self.button_output = QtWidgets.QPushButton(self.formLayoutWidget)
+        self.button_output.setObjectName("button_output")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.button_output)
+        self.label_output = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_output.setObjectName("label_output")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.label_output)
+        self.generate_data_button = QtWidgets.QPushButton(self.centralWidget)
+        self.generate_data_button.setGeometry(QtCore.QRect(40, 210, 75, 23))
+        self.generate_data_button.setObjectName("generate_data_button")
+        self.main_window.setCentralWidget(self.centralWidget)
+
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self.main_window)
+
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.main_window.setWindowTitle(_translate("main_window", "main_window"))
+        self.button_dingdan.setText(_translate("main_window", "导入订单表"))
+        self.label_dingdan.setText(_translate("main_window", "文件路径"))
+        self.label_manjian.setText(_translate("main_window", "文件路径"))
+        self.button_tejia.setText(_translate("main_window", "导入特价表"))
+        self.label_tejia.setText(_translate("main_window", "文件路径"))
+        self.button_manjian.setText(_translate("main_window", "导入满减表"))
+        self.button_output.setText(_translate("main_window", "数据文件输出路径选择"))
+        self.label_output.setText(_translate("main_window", "."))
+        self.generate_data_button.setText(_translate("main_window", "生成数据"))
+
+    def set_status(self, msg):
+        self.main_window.statusBar().showMessage(msg)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = UiMainWindow(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
